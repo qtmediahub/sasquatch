9543add5ab96fd03ac87c1264769a490ee08e940
Girish Ramakrishnan <girish@forwardbias.in>
2010-11-26 13:49:05 +0530

    Add child items of Music menu

--- src/plugins/music/musicplugin.cpp
+++ src/plugins/music/musicplugin.cpp
@@ -2,4 +2,50 @@
 
 #include <QtPlugin>
 
+class MusicPluginItem : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
+
+public:
+    MusicPluginItem(const QString &name, QObject *parent = 0) : QObject(parent), m_name(name) { }
+
+    QString name() const {
+        return m_name;
+    }
+
+    void setName(const QString &name) {
+        m_name = name;
+        emit nameChanged();
+    }
+signals:
+    void nameChanged();
+
+private:
+    QString m_name;
+};
+
+class Files : public QObject
+{
+    Q_OBJECT
+public:
+    Files(QObject *parent = 0) : QObject(parent) { }
+};
+
+MusicPlugin::MusicPlugin()
+{
+        m_childItems << new MusicPluginItem(tr("Files"), this)
+                     << new MusicPluginItem(tr("Add-ons"), this)
+                     << new MusicPluginItem(tr("Library"), this);
+
+}
+
+QList<QObject*> MusicPlugin::childItems() const
+{
+    return m_childItems;
+}
+
 Q_EXPORT_PLUGIN2(music, MusicPlugin)
+
+#include "musicplugin.moc"
+
--- src/plugins/music/musicplugin.h
+++ src/plugins/music/musicplugin.h
@@ -11,10 +11,16 @@ class MusicPlugin : public QObject, public QMHPluginInterface
     Q_INTERFACES(QMHPluginInterface)
 
 public:
+    MusicPlugin();
     ~MusicPlugin() {}
     QString name() const { return tr("Music"); }
     bool browseable() const { return true; }
     QString role() const { return "music"; }
+
+    QList<QObject*> childItems() const;
+
+private:
+    QList<QObject *> m_childItems;
 };
 
 #endif // MUSICPLUGIN_H
