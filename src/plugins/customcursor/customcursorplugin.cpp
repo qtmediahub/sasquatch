08fb3709b04b2fd70234d2983b5079664e080d58
Donald Carr <donald.carr@nokia.com>
2010-12-02 17:34:27 +0000

    Actually check in customcursor

---
+++ src/plugins/customcursor/customcursorplugin.cpp
@@ -0,0 +1,5 @@
+#include "customcursorplugin.h"
+
+#include <QtPlugin>
+
+Q_EXPORT_PLUGIN2(customcursor, CustomCursorPlugin)
---
+++ src/plugins/customcursor/customcursorplugin.h
@@ -0,0 +1,24 @@
+#ifndef CUSTOMCURSORPLUGIN_H
+#define CUSTOMCURSORPLUGIN_H
+
+#include <QObject>
+#include <QDeclarativeItem>
+
+#include "qmhplugininterface.h"
+#include "customcursor.h"
+
+class CustomCursorPlugin : public QObject, public QMHPluginInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(QMHPluginInterface)
+
+public:
+    ~CustomCursorPlugin() {}
+    QString name() const { return tr("CustomCursor"); }
+    bool browseable() const { return true; }
+    QString role() const { return ""; }
+
+    void registerPlugin() { qmlRegisterType<CustomCursor>("CustomCursor", 1, 0, "CustomCursor"); }
+};
+
+#endif // CUSTOMCURSORPLUGIN_H
