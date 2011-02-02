80009fe51f7a446137a9b04702328e1f28e4f785
Girish Ramakrishnan <girish@forwardbias.in>
2011-02-02 19:16:13 +0530

    remotecontrol: set org domain

--- util/remotecontrol/main.cpp
+++ util/remotecontrol/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     app.setApplicationName("QMH Remote Control");
+    app.setOrganizationDomain("nokia.com");
     app.setOrganizationName("Nokia");
 
     MainWindow mainWindow;
