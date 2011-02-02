f006ce2c7ddd7759f1c000f7b20443b4f5965ae8
Girish Ramakrishnan <girish@forwardbias.in>
2011-02-02 17:34:34 +0530

    remotecontrol: set the app name and org name

--- util/remotecontrol/main.cpp
+++ util/remotecontrol/main.cpp
@@ -4,6 +4,8 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    app.setApplicationName("QMH Remote Control");
+    app.setOrganizationName("Nokia");
 
     MainWindow mainWindow;
 #if defined(Q_OS_SYMBIAN) || defined(Q_WS_MAEMO5)
