4902040bbcb6f04731448c364946fe46d95db9df
Johannes Zellner <johannes.zellner@nokia.com>
2011-02-04 17:46:59 +0100

    wrong include

--- util/qmlremotecontrol/staticservicebrowsermodel.cpp
+++ util/qmlremotecontrol/staticservicebrowsermodel.cpp
@@ -17,7 +17,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 
 ****************************************************************************/
 
-#include "staticservicebrowserview.h"
+#include "staticservicebrowsermodel.h"
 #include <QtGui>
 
 StaticServiceBrowserModel::StaticServiceBrowserModel(QWidget *parent)
