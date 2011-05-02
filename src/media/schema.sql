e6ffcdea03069f9556154d1a303b6bee343e02d4
Girish Ramakrishnan <girish@forwardbias.in>
2011-05-02 08:39:44 +0530

    Add missing schema.sql

---
+++ src/media/schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE directories (
+    path TEXT NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE music (
+    id INTEGER PRIMARY KEY, /* alias for ROWID, see http://www.sqlite.org/autoinc.html */
+    filepath TEXT NOT NULL UNIQUE,
+
+    /* taglib */
+    title TEXT,
+    album TEXT,
+    artist TEXT,
+    track INTEGER,
+    year INTEGER,
+    genre TEXT,
+    comment TEXT,
+    thumbnail BLOB,
+
+    length INTEGER,
+    bitrate INTEGER,
+    samplerate INTEGER,
+
+    /* stat information */
+    directory TEXT NOT NULL,
+    mtime INTEGER NOT NULL,
+    ctime INTEGER NOT NULL,
+    filesize INTEGER NOT NULL
+);
+
+CREATE INDEX idx_album ON music (album);
+
+CREATE INDEX idx_artist ON music (artist);
+
