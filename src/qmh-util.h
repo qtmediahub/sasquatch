b5472cd360bb91ebdc8f226bd14a22dcccdb18c1
Donald Carr <donald.carr@nokia.com>
2011-07-28 05:32:30 +0000

    Disable shine-through by default
    Performance degradation seen on Ion board when run without compositor!!!!
    
    Wholly anecdotal evidence to support this

--- src/qmh-util.h
+++ src/qmh-util.h
@@ -2,13 +2,14 @@
 #define QMHUTIL_H
 
 #include <QWidget>
+#include "qmh-config.h"
 
 namespace Utils
 {
     static void optimizeWidgetAttributes(QWidget *widget, bool transparent = false) {
         widget->setAttribute(Qt::WA_OpaquePaintEvent);
         widget->setAutoFillBackground(false);
-        if (transparent)
+        if (transparent && Config::isEnabled("shine-through", false))
             widget->setAttribute(Qt::WA_TranslucentBackground);
         else
             widget->setAttribute(Qt::WA_NoSystemBackground);
