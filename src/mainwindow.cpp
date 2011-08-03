3c1e7352cb28e872647d180d6a78c9d710be58a4
Girish Ramakrishnan <girish@forwardbias.in>
2011-08-03 23:25:36 +0530

    Remove < 4.7.2 logic. We require atleast 4.7.4 anyway

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -87,19 +87,6 @@ void MainWindow::setOrientation(ScreenOrientation orientation)
 
     Qt::WidgetAttribute attribute;
     switch (orientation) {
-#if QT_VERSION < 0x040702
-    // Qt < 4.7.2 does not yet have the Qt::WA_*Orientation attributes
-    case ScreenOrientationLockPortrait:
-        attribute = static_cast<Qt::WidgetAttribute>(128);
-        break;
-    case ScreenOrientationLockLandscape:
-        attribute = static_cast<Qt::WidgetAttribute>(129);
-        break;
-    default:
-    case ScreenOrientationAuto:
-        attribute = static_cast<Qt::WidgetAttribute>(130);
-        break;
-#else // QT_VERSION < 0x040702
     case ScreenOrientationLockPortrait:
         attribute = Qt::WA_LockPortraitOrientation;
         break;
@@ -110,7 +97,6 @@ void MainWindow::setOrientation(ScreenOrientation orientation)
     case ScreenOrientationAuto:
         attribute = Qt::WA_AutoOrientation;
         break;
-#endif // QT_VERSION < 0x040702
     };
     setAttribute(attribute, true);
 }
