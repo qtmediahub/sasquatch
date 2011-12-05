fe0f8562dc0505ab6483c46ec228f42f300597f1
Johannes Zellner <johannes.zellner@nokia.com>
2011-12-05 15:38:31 +0100

    dont show networking errors by default

--- src/media/plugins/music/lastfmprovider.cpp
+++ src/media/plugins/music/lastfmprovider.cpp
@@ -111,7 +111,7 @@ void LastFMProvider::requestNext()
 
 void LastFMProvider::handleError(QNetworkReply::NetworkError code)
 {
-    qWarning() << "cannot fetch lastFM data" << m_reply->errorString();
+    DEBUG << "cannot fetch lastFM data" << m_reply->errorString();
 
     m_reply->deleteLater();
     m_reply = 0;
